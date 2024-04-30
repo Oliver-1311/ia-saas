df3cfe7c7a06d9b6acb81adc949d852f106ac49a
Oliver Macedo <oli932300102@gmail.com>
2024-04-29 22:03:58 -0500

    add: routing and project structure

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
-  return <div>layout</div>;
+  return <main className="auth">{children}</main>;
 };
 
 export default layout;
---
+++ src/app/(root)/transformation/[id]/update/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UpdateTransformationIdPage = () => {
+  return <div>UpdateTransformationIdPage</div>;
+};
+
+export default UpdateTransformationIdPage;
